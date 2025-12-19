7ac1d0fb055b74aee530e2d31fcfe40a2c9b1038
renigoms <renan.nic28@gmail.com>
2025-12-19 10:39:43 -0300

    feat: enviando mesagens para o kafka

---
+++ str-producer/src/main/java/org/renigoms/strproducer/config/StringProducerFactoryConfig.java
@@ -0,0 +1,33 @@
+package org.renigoms.strproducer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+
+@Configuration
+@RequiredArgsConstructor
+public class StringProducerFactoryConfig {
+    private final KafkaProperties properties;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        HashMap<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
