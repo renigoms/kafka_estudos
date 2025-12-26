33bc90b558004a11f4694ae883f459f50afad03e
renigoms <renan.nic28@gmail.com>
2025-12-25 22:28:37 -0300

    feat: add validations

--- str-consumer/src/main/java/org/renigoms/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/org/renigoms/strconsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package org.renigoms.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
+import org.renigoms.strconsumer.custom.StrConsumerCustomListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,17 +9,18 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-00", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-01")
     public void create(String message){
         log.info("CREATE ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-01", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-01")
     public void log(String message){
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-02", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-02",
+            topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message){
         log.info("HISTORY ::: Receive message {}", message);
     }
