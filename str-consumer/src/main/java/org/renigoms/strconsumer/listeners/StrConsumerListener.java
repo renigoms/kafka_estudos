fb951c5ecd5d3fe51563b91ad8e99c6bfbd03d7b
renigoms <renan.nic28@gmail.com>
2026-01-12 19:58:23 -0300

    feat: error handler

--- str-consumer/src/main/java/org/renigoms/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/org/renigoms/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package org.renigoms.strconsumer.listeners;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.renigoms.strconsumer.custom.StrConsumerCustomListener;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
+//    Mesma coisa que fazer throws Exception no método
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-01")
     public void create(String message){
         log.info("CREATE ::: Receive message {}", message);
@@ -17,6 +20,7 @@ public class StrConsumerListener {
     @StrConsumerCustomListener(groupId = "group-01")
     public void log(String message){
         log.info("LOG ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
     }
 
     @KafkaListener(groupId = "group-02",
